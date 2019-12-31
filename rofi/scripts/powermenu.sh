88a412813a11e683f135dbb235b72f1a91ec9d7d
Mahmoudk1000 <dodok1000@gmail.com>
2019-12-31 22:05:50 +0200

    remove logout

--- rofi/scripts/powermenu.sh
+++ rofi/scripts/powermenu.sh
@@ -2,33 +2,30 @@
 
 # options to be displayed
 option0="lock"
-option1="logout"
-option2="suspend"
-option3="scheduled suspend (10min)"
-option4="scheduled suspend (20min)"
-option5="scheduled suspend (30min)"
-option6="reboot"
-option7="shutdown"
+option1="suspend"
+option2="scheduled suspend (10min)"
+option3="scheduled suspend (20min)"
+option4="scheduled suspend (30min)"
+option5="reboot"
+option6="shutdown"
 
 # options passed into variable
-options="$option0\n$option1\n$option2\n$option3\n$option4\n$option5\n$option6\n$option7"
+options="$option0\n$option1\n$option2\n$option3\n$option4\n$option5\n$option6"
 
 chosen="$(echo -e "$options" | rofi -lines 8 -dmenu -p "power")"
 case $chosen in
     $option0)
         i3lock;;
     $option1)
-        i3-msg exit;;
-    $option2)
         systemctl suspend;;
-	$option3)
+	$option2)
 		sleep 600 && systemctl suspend;;
-	$option4)
+	$option3)
 		sleep 1200 && systemctl suspend;;
-	$option5)
+	$option4)
 		sleep 1800 && systemctl suspend;;
-    $option6)
+    $option5)
         systemctl reboot;;
-	$option7)
+	$option6)
         systemctl poweroff;;
 esac
